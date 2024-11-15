c6c141da3bf7d87b2682ae914d7884df41fbeb24
Ambe Hideki <ykc@daycrift.net>
2024-11-16 00:30:19 +0900

    Feat: Rename and refactor dictionary class
    Renamed the class and file from Japanese to English for consistency. Simplified the logic for searching, adding, and displaying words, and added new methods for handling word registration.

--- lib/tangome.rb
+++ lib/tangome.rb
@@ -1,23 +1,13 @@
-require_relative '../lib/tangome/単語辞書'
+require_relative '../lib/tangome/dictionary'
 
-辞書 = Class単語辞書.new
+dictionary = Dictionary.new
 
 if ARGV.length > 0
-  if ARGV[0] == 'add' && !ARGV[1].nil?
-    結果 = 辞書.検索(ARGV[1])
-    if 結果.is_a?(String)
-      puts 'すでに登録されています'
-      辞書.表示(結果)
-      return
-    end
-
-    辞書.追加(ARGV[1])
-  elsif ARGV[0] == 'dic'
-    puts 辞書.直接編集
+  if ARGV[0] == 'dic'
+    puts dictionary.dictionary_path
+  elsif ARGV[0] == 'all'
+    dictionary.list_all
   else
-    単語 = 辞書.検索(ARGV[0])
-    辞書.表示(単語)
+    dictionary.search(ARGV[0])
   end
-else
-  辞書.一覧
 end
---
+++ lib/tangome/dictionary.rb
@@ -0,0 +1,108 @@
+require 'open-uri'
+require 'nokogiri'
+require 'toml-rb'
+
+class Dictionary
+  CONST_DICTIONARY_PATH = File.expand_path('../../dictionary.toml', __dir__).freeze
+
+  def initialize
+    @dictionary = load_dictionary
+  end
+
+  attr_accessor :dictionary
+
+  # editor で開く用に辞書ファイルのパスを返します
+  def dictionary_path
+    CONST_DICTIONARY_PATH
+  end
+
+  def search(word)
+    result = search_local(word)
+    if result && result.is_a?(String)
+      show(word)
+      return
+    end
+
+    result = search_weblio(word)
+    unless result.empty?
+      add(word, result)
+      show(word)
+      return
+    end
+
+    show('')
+    add_custom_word_or_nothing(word)
+  end
+
+  def list_all
+    dictionary.each do |word, explain|
+      show(word)
+    end
+  end
+
+  def add(word, explain = nil)
+    unless explain
+      explain = search_weblio(word)
+      explain = 'TBD' if explain.empty?
+    end
+    dictionary[word] = explain
+
+    save_dictionary
+  end
+
+  def add_custom_word_or_nothing(word)
+    puts "単語を登録しますか？(y/n)"
+    answer = STDIN.gets.chomp
+    if answer == 'y'
+      explain = STDIN.gets.chomp
+      if explain
+        add(word, explain)
+        show(word)
+      end
+    end
+  end
+
+  def show(word)
+    explain = dictionary[word]
+    if explain.nil?
+      puts "単語が見つかりません．"
+    elsif word.is_a?(String)
+      puts "#{word}: #{dictionary[word]}"
+    else
+      puts "候補が見つかりました(先頭から 5 件表示します)"
+      word.take(5).each do |w|
+        puts "#{w}: #{dictionary[w]}"
+      end
+    end
+  end
+
+  private
+
+  def load_dictionary
+    begin
+      TomlRB.load_file(CONST_DICTIONARY_PATH)
+    rescue Errno::ENOENT
+      File.new(CONST_DICTIONARY_PATH, 'w')
+      TomlRB.load_file(CONST_DICTIONARY_PATH)
+    end
+  end
+
+  def save_dictionary
+    File.open(CONST_DICTIONARY_PATH, 'w+') { |file| file.write(TomlRB.dump(dictionary)) }
+  end
+
+  def search_local(word)
+    result = dictionary.keys.find { _1 == word }
+    result ||= dictionary.keys.filter { _1.include?(word) }
+    result || ''
+  end
+
+  def search_weblio(word)
+    url = "https://ejje.weblio.jp/content/#{URI.encode_www_form_component(word)}"
+    doc = Nokogiri::HTML(URI.open(url), nil, 'utf-8')
+    text = doc.at_css('#summary .content-explanation')&.text
+    text&.strip || ''
+  rescue OpenURI::HTTPError
+    ''
+  end
+end
